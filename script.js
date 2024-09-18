f25de5832a4151abb66f554233a422df30561443
markovhome <marck.pasha3@gmail.com>
2024-09-18 21:14:40 +0300

    Update index.html, script.js, and styles.css add API key, status and currencies requests

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+let btn = document.querySelector("#request-btn");
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", function () { console.log(this.responseText); });
+oReq.open("GET", "https://api.freecurrencyapi.com/v1/status?apikey=fca_live_qqMcXgtYmpYcOU8pn6KYZGEOOYnvNN8LOdwMH9Dg");
+oReq.send();
+
+const currenciesRequest = () => {
+    oReq.open("GET", "https://api.freecurrencyapi.com/v1/currencies?apikey=fca_live_qqMcXgtYmpYcOU8pn6KYZGEOOYnvNN8LOdwMH9Dg&currencies=EUR%2CUSD%2CCAD");
+    oReq.send();
+}
+
+btn.addEventListener("click", currenciesRequest);
